8d4b49940f1524dfd850bd55948d04eb5d19ecdf
ishaq <229y1a05h4@ksrmce.ac.in>
2026-02-14 20:36:18 +0530

    Add backend and React frontend

--- fake-news-ui/vite.config.js
+++ fake-news-ui/vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // ✅ for custom domain root
+  base: "/Fake-News-Detection-System/",  // ✅ IMPORTANT
 });
