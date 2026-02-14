175ffe4a2cd0668d4b466ff2a9bbc263fae3ed00
ishaq <229y1a05h4@ksrmce.ac.in>
2026-02-14 20:03:47 +0530

    Add backend and React frontend

--- fake-news-ui/vite.config.js
+++ fake-news-ui/vite.config.js
@@ -3,9 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",                 // ✅ custom domain root
-  build: {
-    outDir: "../docs",       // ✅ output for GitHub Pages
-    emptyOutDir: true
-  }
+  base: "/", // ✅ custom domain root
 });
